fa92304b56f71f3d13c5c74afa9a367485b0aa46
Avinash <avinashchandrabarik@gmail.com>
2023-09-29 00:13:36 +0530

    HACKTOBERFEST-ACCEPTED

--- src/components/Feature.js
+++ src/components/Feature.js
@@ -1,84 +1,71 @@
 import React, { useEffect } from "react";
-import "./Feature.css";
+import { Button } from "@mui/material";
+import Aos from "aos";
+import "aos/dist/aos.css";
+
 import F1 from "../assets/F1.png";
 import F2 from "../assets/F2.png";
 import F3 from "../assets/F3.png";
 import F4 from "../assets/F4.png";
 import F5 from "../assets/F5.png";
-import { Button } from "@mui/material";
-import Aos from "aos";
-import "aos/dist/aos.css";
-function Feature() {
+
+import "./Feature.css";
+
+const Feature = () => {
   useEffect(() => {
     Aos.init({
       offset: 300,
     });
   }, []);
+
+  const featureItems = [
+    {
+      imgSrc: F1,
+      title: "Technology Reports",
+      description:
+        "Download lists of websites using a specific technology. We track over 3,000 technologies across 280+ million websites.",
+    },
+    {
+      imgSrc: F2,
+      title: "Rich Data",
+      description:
+        "Technology Reports include email, phone numbers, and website profiling metadata like IP, Hostname, DNS, titles, and other information.",
+    },
+    {
+      imgSrc: F3,
+      title: "Email Verification",
+      description:
+        "Industry-leading 9-step email verification engine. Never worry about email non-delivery again!",
+    },
+    {
+      imgSrc: F4,
+      title: "Zone Files",
+      description:
+        "Download lists of active domains across 100s of gTLDs and ccTLDs updated daily.",
+    },
+    {
+      imgSrc: F5,
+      title: "Deep Search",
+      description:
+        "Custom deploy our technology to search domains for contact information.",
+    },
+  ];
+
   return (
     <div className="feature">
       <div className="fHead">Power Your Sales And Marketing Teams</div>
       <div className="fIcons">
-        <div className="fI">
-          <div className="fIcon" data-aos="fade-up">
-            <div className="fIconImg">
-              <img src={F1} alt="" />
-            </div>
-            <div className="fIconHead">Technology Reports</div>
-            <div className="fIconTitle">
-              Download lists of websites using a specific technology. We track
-              over 3,000 technologies across 280+ million websites.
-            </div>
-          </div>
-        </div>
-        <div className="fI">
-          <div className="fIcon" data-aos="fade-up">
-            <div className="fIconImg">
-              <img src={F2} alt="" />
-            </div>
-            <div className="fIconHead">Rich Data</div>
-            <div className="fIconTitle">
-              Technology Reports include email, phone numbers, and website
-              profiling metadata like IP, Hostname, DNS, titles, and other
-              information.
+        {featureItems.map((item, index) => (
+          <div className="fI" key={index}>
+            <div className="fIcon" data-aos="fade-up">
+              <div className="fIconImg">
+                <img src={item.imgSrc} alt="" />
+              </div>
+              <div className="fIconHead">{item.title}</div>
+              <div className="fIconTitle">{item.description}</div>
             </div>
           </div>
-        </div>
-        <div className="fI">
-          <div className="fIcon" data-aos="fade-up">
-            <div className="fIconImg">
-              <img src={F3} alt="" />
-            </div>
-            <div className="fIconHead">Email Verification</div>
-            <div className="fIconTitle">
-              Industry-leading 9-step email verification engine. Never worry
-              about email non-delivery again!
-            </div>
-          </div>
-        </div>{" "}
-        <div className="fI">
-          <div className="fIcon" data-aos="fade-up">
-            <div className="fIconImg">
-              <img src={F4} alt="" />
-            </div>
-            <div className="fIconHead">Zone Files</div>
-            <div className="fIconTitle">
-              Download lists of active domains across 100s of gTLDs and ccTLDs
-              updated daily.
-            </div>
-          </div>
-        </div>{" "}
-        <div className="fI">
-          <div className="fIcon" data-aos="fade-up">
-            <div className="fIconImg">
-              <img src={F5} alt="" />
-            </div>
-            <div className="fIconHead">Deep Search</div>
-            <div className="fIconTitle">
-              Custom deploy our technology to search domains for contact
-              information.
-            </div>
-          </div>
-        </div>
+        ))}
       </div>
       <div className="fButton">
         <div className="fBut">
@@ -90,6 +77,6 @@ function Feature() {
       </div>
     </div>
   );
-}
+};
 
 export default Feature;
